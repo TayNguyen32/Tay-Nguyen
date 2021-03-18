71e9ca4a04b6241dc93b0e6f7ab1e9808c5d0901
TayNguyen32 <75840183+TayNguyen32@users.noreply.github.com>
2021-03-18 02:14:57 -0700

    adding media query to fix section 2 also had gotten rid of unnecessary js code

--- js/scrips.js
+++ js/scrips.js
@@ -1,18 +1,5 @@
 console.log("ok");
 
-// Store Background Classes
-const backgrounds = ["bg0", "bg1", "bg2"];
-
-// Get Random Number
-var numRand;
-numRand = Math.floor(Math.random() * backgrounds.length);
-
-// Get element
-let pageBackground = document.querySelector("body");
-
-// Set class name from stored values
-pageBackground.className = backgrounds[numRand];
-
 // portfolio setup
 const portfolio = document.querySelector("#portfolio");
 const video = document.getElementById("video");
