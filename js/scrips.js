bf99334e88cc2231a70100ac9bb226927c322bfb
TayNguyen32 <75840183+TayNguyen32@users.noreply.github.com>
2021-07-31 04:42:56 -0700

    Newly updated Added 4 new thumbnails and pictures updated 2 2 previous ones

--- js/scrips.js
+++ js/scrips.js
@@ -2,9 +2,22 @@ console.log("ok");
 
 // portfolio setup
 const portfolio = document.querySelector("#portfolio");
+const portfolio2 = document.querySelector("#portfolio2");
 const video = document.getElementById("video");
 
 lightGallery(portfolio, {
+  selector: "a",
+  mode: "lg-slide-circular",
+  download: false,
+  cssEasing: "ease",
+  startClass: "lg-start-zoom",
+  subHtmlSelectorRelative: true,
+  zoom: true,
+  scale: 0.5,
+  thumbnail: false
+});
+
+lightGallery(portfolio2, {
   selector: "a",
   mode: "lg-slide-circular",
   download: false,
